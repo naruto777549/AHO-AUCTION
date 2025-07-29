e469f9b00207eb889780f75815d2fb22fa67d166
naruto777549 <shekhikrar026@gmail.com>
2025-07-29 06:42:05 +0530

    Update start.py

--- Auction/Modules/start.py
+++ Auction/Modules/start.py
@@ -1,1 +1,9 @@
-#
+import json
+import telebot
+from telebot import types
+import threading
+import time
+import re
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import InputMediaPhoto
+from jinja2 import Template
