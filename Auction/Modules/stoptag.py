7627e05a8d2613c28dfd42d49ec3d7f6e9570c21
naruto777549 <shekhikrar026@gmail.com>
2025-07-30 01:08:37 +0530

    Create stoptag.py

---
+++ Auction/Modules/stoptag.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from config import bot
+from Database.state import active_tags
+
+@bot.on_message(filters.command("stoptag") & filters.group)
+async def stop_tag(_, message: Message):
+    chat_id = message.chat.id
+
+    if chat_id in active_tags:
+        active_tags.pop(chat_id)
+        await message.reply("🛑 Tagging stopped.")
+    else:
+        await message.reply("⚠️ No ongoing tag process.")
