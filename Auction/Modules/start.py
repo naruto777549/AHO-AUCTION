4f9be0462cd4f7a339b602622fa6639a87575fcd
naruto777549 <shekhikrar026@gmail.com>
2025-07-30 01:37:02 +0530

    Update start.py

--- Auction/Modules/start.py
+++ Auction/Modules/start.py
@@ -1,7 +1,24 @@
 from pyrogram import filters
-from pyrogram.types import Message
-from Auction import bot
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from Auction import bot  
 
 @bot.on_message(filters.command("start") & filters.private)
 async def start(_, message: Message):
-    await message.reply("👋 Welcome! Use /help to see commands.")
+    await message.reply_text(
+        """🌀 ᴛᴀɢᴀʟʟ ʙᴏᴛ
+➖➖➖➖➖➖➖➖➖➖➖➖
+‣ ᴀᴜᴛᴏ-ᴛᴀɢ ᴀʟʟ ɢʀᴏᴜᴘ ᴍᴇᴍʙᴇʀs ɪɴ ᴄʜᴜɴᴋs
+‣ ᴜsᴇ /tagall ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴇᴠᴇʀʏᴏɴᴇ
+‣ sᴜᴘᴘᴏʀᴛs ʀᴇᴘʟʏ + ᴄᴜsᴛᴏᴍ ᴍᴇssᴀɢᴇ
+‣ sᴛᴏᴘ ᴛᴀɢ ᴀɴʏᴛɪᴍᴇ ᴜsɪɴɢ /stoptag
+➖➖➖➖➖➖➖➖➖➖➖➖
+ᴇᴀsʏ ᴛᴏ ᴜsᴇ & ғᴜʟʟʏ ғᴜɴᴄᴛɪᴏɴᴀʟ ᴛᴀɢɢɪɴɢ ʙᴏᴛ ғᴏʀ ɢʀᴏᴜᴘs 🚀""",
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("➕ Add me to your Group", url=f"https://t.me/{bot.me.username}?startgroup=true")],
+            [
+                InlineKeyboardButton("👑 Owner", url="https://t.me/Uzumaki_X_Naruto_6"),
+                InlineKeyboardButton("💬 Support Chat", url="https://t.me/Aho_Hexa_Auction")
+            ]
+        ]),
+        disable_web_page_preview=True
+    )
