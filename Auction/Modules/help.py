279142ac20ca737e8b9d888b324c820f02a27a6a
naruto777549 <shekhikrar026@gmail.com>
2025-07-30 01:06:58 +0530

    Create help.py

---
+++ Auction/Modules/help.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from config import bot
+
+@bot.on_message(filters.command("help"))
+async def help_cmd(_, message: Message):
+    await message.reply("""
+**🤖 Bot Commands:**
+/start - Start the bot
+/help - Show this message
+/tagall [msg] - Tag all users (GC only)
+/stoptag - Stop ongoing tag
+/bcast - Broadcast (Admins only)
+    """)
