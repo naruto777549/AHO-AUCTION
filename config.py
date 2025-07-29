9781b3e5f943b3e8b04c0c5e7654beabcd465042
naruto777549 <shekhikrar026@gmail.com>
2025-07-30 02:32:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 API_ID = int(os.getenv("API_ID", "21218274"))
 API_HASH = os.getenv("API_HASH", "3474a18b61897c672d315fb330edb213")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7956858405:AAH_uXebrWLkZagsWsCAeyFLD1EAuNfiVe4")
+
+ADMINS = list(map(int, os.getenv("ADMINS", "7576729648,6642049252").split(",")))
