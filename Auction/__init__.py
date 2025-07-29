203d5320ab0598f6559827051a52acec714ac209
naruto777549 <shekhikrar026@gmail.com>
2025-07-29 06:38:31 +0530

    Update __init__.py

--- Auction/__init__.py
+++ Auction/__init__.py
@@ -1,1 +1,13 @@
 # __init__.py
+from datetime import datetime
+start_time = datetime.utcnow()
+
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+bot = Client(
+    "All_in_one",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,       
+)
