463c1e01a2ba1e3fad5c4d1f6877d5a476b57567
naruto777549 <shekhikrar026@gmail.com>
2025-07-30 01:17:54 +0530

    Update bcast.py

--- Auction/Modules/bcast.py
+++ Auction/Modules/bcast.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from config import bot
+from Auction import bot
 
 @bot.on_message(filters.command("bcast") & filters.private)
 async def broadcast(_, message: Message):
