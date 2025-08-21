5e0d516c117d7a5552a7caac58da8d89af9bc7ce
naruto777549 <shekhikrar026@gmail.com>
2025-08-22 01:14:56 +0530

    Update start.py

--- Auction/Modules/start.py
+++ Auction/Modules/start.py
@@ -3,26 +3,36 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from Auction import bot
 from Auction.db import save_user, save_group
 
+
 # ✅ Handle /start in private chat
 @bot.on_message(filters.command("start") & filters.private)
 async def start_private(_, message: Message):
     await save_user(message.from_user.id)
     me = await bot.get_me()
 
-    await message.reply_text(
-        """🌀 ᴛᴀɢᴀʟʟ ʙᴏᴛ
-➖➖➖➖➖➖➖➖➖➖➖➖
-‣ ᴀᴜᴛᴏ-ᴛᴀɢ ᴀʟʟ ɢʀᴏᴜᴘ ᴍᴇᴍʙᴇʀs ɪɴ ᴄʜᴜɴᴋs
-‣ ᴜsᴇ /tagall ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴇᴠᴇʀʏᴏɴᴇ
-‣ sᴜᴘᴘᴏʀᴛs ʀᴇᴘʟʏ + ᴄᴜsᴛᴏᴍ ᴍᴇssᴀɢᴇ
-‣ sᴛᴏᴘ ᴛᴀɢ ᴀɴʏᴛɪᴍᴇ ᴜsɪɴɢ /stoptag
-➖➖➖➖➖➖➖➖➖➖➖➖
-ᴇᴀsʏ ᴛᴏ ᴜsᴇ & ғᴜʟʟʏ ғᴜɴᴄᴛɪᴏɴᴀʟ ᴛᴀɢɢɪɴɢ ʙᴏᴛ ғᴏʀ ɢʀᴏᴜᴘs 🚀""",
+    await message.reply_photo(
+        photo="https://files.catbox.moe/9worhw.jpg",
+        caption="""┏━━━━━━━━━━━━━━━━━━━━━━━━━⧫
+✾ Wᴇʟᴄᴏᴍᴇ ᴛᴏ AHO | Tagger
+┗━━━━━━━━━━━━━━━━━━━━━━━━━⧫
+┏━━━━━━━━━━━━━━━━━━━━━━━━━⧫
+┠ ➻ Aᴜᴛᴏ-Tᴀɢ ᴀʟʟ Gʀᴏᴜᴘ Mᴇᴍʙᴇʀs
+┃      Fᴀsᴛ & Rᴇʟɪᴀʙʟᴇ
+┠ ➻ Cᴜsᴛᴏᴍ Mᴇssᴀɢᴇ + Rᴇᴘʟʏ Sᴜᴘᴘᴏʀᴛ
+┃      Fᴜʟʟʏ Fᴜɴᴄᴛɪᴏɴᴀʟ
+┠ ➻ Sᴛᴏᴘ ᴛᴀɢ ᴀɴʏᴛɪᴍᴇ ᴜsɪɴɢ /stoptag
+┗━━━━━━━━━━━━━━━━━━━━━━━━━⧫
+
+🚀 Pᴏᴡᴇʀғᴜʟ ᴛᴀɢɢɪɴɢ ʙᴏᴛ ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘs!""",
         reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ", url=f"https://t.me/{me.username}?startgroup=true")],
             [
-                InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/Uzumaki_X_Naruto_6"),
+                InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/God_X_Pawan"),
                 InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/Aho_Hexa_Auction")
+            ],
+            [
+                InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Uzumaki_X_Naruto_6"),
+                InlineKeyboardButton("ᴀᴄᴛɪᴏɴ ɢᴄ", url="https://t.me/+3GuE8k8XsuJhODY1")
             ]
         ]),
         disable_web_page_preview=True
