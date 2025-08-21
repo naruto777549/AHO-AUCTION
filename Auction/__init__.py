6c5e1d684f3df812d4575aabac634a460fa742ef
naruto777549 <shekhikrar026@gmail.com>
2025-08-22 01:04:34 +0530

    Update __init__.py

--- Auction/__init__.py
+++ Auction/__init__.py
@@ -1,13 +1,14 @@
-# __init__.py
 from datetime import datetime
-start_time = datetime.utcnow()
-
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
+# Bot uptime ka track
+start_time = datetime.utcnow()
+
+# Pyrogram Client
 bot = Client(
     "All_in_one",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN,       
+    bot_token=BOT_TOKEN,
 )
