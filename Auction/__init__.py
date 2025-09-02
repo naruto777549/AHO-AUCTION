0740d50d9baa4d747720cd285f961075cedd0698
naruto777549 <shekhikrar026@gmail.com>
2025-09-03 01:02:10 +0530

    Update __init__.py

--- Auction/__init__.py
+++ Auction/__init__.py
@@ -1,1 +1,11 @@
 # __init__.py
+# Auction/__init__.py
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+app = Client(
+    "TAGGER_BOT",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
